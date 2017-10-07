32079d09f0d738d14b76b19736ae91536d5e5cf8
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-10-08 00:43:19 +0330

    @ js/load_objects.js: improved

--- js/load_objects.js
+++ js/load_objects.js
@@ -1,33 +1,41 @@
-function load_objects(models, callback) {
-  objects = {}
-  for (var i = 0; i < models.length; i++) {
-    setTimeout(function(model, id) {
-    	var texture = new THREE.Texture();
-    	var loader = new THREE.ImageLoader();
-    	loader.load(model.texture, function ( image ) {
-    		texture.image = image;
-    		texture.needsUpdate = true;
-        // repeat the texture
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    	});
+function load_texture(file) {
+  var texture = new THREE.Texture();
+  var loader = new THREE.ImageLoader();
+  loader.load(file, function ( image ) {
+    texture.image = image;
+    texture.needsUpdate = true;
+    // repeat the texture
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  });
+  return texture;
+}
 
-      var loader = new THREE.OBJLoader();
+function set_texture(object, texture) {
+  object.traverse( function ( child ) {
+    if ( child instanceof THREE.Mesh ) {
+      child.material.map = texture;
+    }
+  });
+}
+
+function load_objects(models, callback) {
+  var objects = {}
+  for (var name in models) {
+    console.log(name);
+    setTimeout(function(model, id) {
+      var texture = load_texture(model.texture);
     	// load a resource
-    	loader.load(
+    	 new THREE.OBJLoader().load(
     		// resource URL
     		model.file,
     		// Function when resource is loaded
     		function ( object ) {
-    			object.traverse( function ( child ) {
-    				if ( child instanceof THREE.Mesh ) {
-    					child.material.map = texture;
-    				}
-    			});
           objects[id] = object;
-          if(Object.keys(objects).length === models.length && typeof callback === "function")
+          set_texture(object, texture)
+          if(Object.keys(objects).length === Object.keys(models).length && typeof callback === "function")
             callback(objects)
     		}
     	);
-    }, 10, models[i], i);
+    }, 10, models[name], name);
   }
 }
